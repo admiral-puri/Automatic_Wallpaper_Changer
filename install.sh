160de0b0fefe3d5fab6cb6d96d86d1f2ab155cd2
Suraj SIngh Rana <surajsinghrana7417@gmail.com>
2019-08-06 23:41:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,17 +7,23 @@ echo "installing"
 cd ../
 
 user=$(whoami)
-
 echo $user
-
 sudo cp -r Automatic_Wallpaper_Changer  /home/$user/
 
 crontab -l > new_cron
-
 echo "@reboot /home/"$user"/Automatic_Wallpaper_Changer/executer.sh" > new_cron
-
 crontab new_cron
 
-echo "reboot to start service"
+echo "main installation is complete"
+echo "now installing python dependencies"
 
+
+pip3 getpass
+pip3 random  
+pip3 install requests
+pip3 install UnsplashSearch 
+pip3 install os
+
+
+echo "reboot to start service"
 echo "program configuration completed"
